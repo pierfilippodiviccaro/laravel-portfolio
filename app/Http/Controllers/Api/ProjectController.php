b6f1dc79e45db67e1ca46de82b8039ee7d87fab6
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2026-04-08 21:06:36 +0200

    feat: implementato il controller e le route per la chiamata ad un api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects=Project::all();
+        return response()->json([
+            'success'=>true,
+            'data'=>$projects,
+        ]);
+        
+    }
+
+
+
+    public function show(Project $project){
+        $project->load('type','technologies');
+        return response()->json([
+            "success"=>true,
+            "data"=>$project
+        ]);
+    }
+}
